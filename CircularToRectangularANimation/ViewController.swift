8fbca8c38f682e4587f039523c478760f43316a9
Daniel Karath <karath.dani@gmail.com>
2023-01-15 13:23:18 +0100

    Initial Commit

---
+++ CircularToRectangularANimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CircularToRectangularANimation
+//
+//  Created by Daniel Karath on 1/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
